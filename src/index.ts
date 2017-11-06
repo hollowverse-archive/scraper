0f0600766ea77be3ec3eb8ef24be08ab6bdc7582
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-06 21:28:37 +0200

    Support URL base

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,27 @@ import writeJsonFile from '@hollowverse/common/helpers/writeJsonFile';
 import { scrapePage } from './lib/scrape';
 import { URL } from 'url';
 
+const defaultBase = 'https://static.hollowverse.com';
+
 program
   .version('1.0')
   .option(
     '-p --path <path>',
     'The path of the URL to scrape without the leading slash, e.g. tom-hanks',
   )
+  .option(
+    '-b --base [base]',
+    `The hostname of the website to be scraped, defaults to ${defaultBase}`,
+  )
   .output(
     '-o --output [output]',
-    'If specificed, the output data will be written to the specified path, ' +
+    'If specified, the output data will be written to the specified path, ' +
       'otherwise, output is written to stdout',
   )
   .parse(process.argv);
 
 scrapePage({
-  url: new URL(program.path, 'https://static.hollowverse.com').toString(),
+  url: new URL(program.path, program.base || defaultBase).toString(),
 })
   .then(output => JSON.stringify(output.data))
   .then(async json => {
